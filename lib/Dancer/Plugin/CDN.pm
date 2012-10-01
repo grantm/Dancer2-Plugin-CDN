f9106a66b00d6c2b31c88a6afa8d4a6f25ed4860
Grant McLean <grant@mclean.net.nz>
2012-10-01 21:29:10 +1300

    initial import of dzil skeleton

---
+++ lib/Dancer/Plugin/CDN.pm
@@ -0,0 +1,104 @@
+package Dancer::Plugin::CDN;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Dancer::Plugin::CDN - The great new Dancer::Plugin::CDN!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Dancer::Plugin::CDN;
+
+    my $foo = Dancer::Plugin::CDN->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-Dancer::Plugin::CDN at rt.cpan.org>,
+or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Dancer::Plugin::CDN>.  I will be
+notified, and then you'll automatically be notified of progress on your bug as
+I make changes.
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Dancer::Plugin::CDN
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Dancer::Plugin::CDN>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Dancer::Plugin::CDN>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Dancer::Plugin::CDN>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Dancer::Plugin::CDN/>
+
+=item * Source Code Repository
+
+L<http://github.com/grantm/Dancer::Plugin::CDN/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2011 Grant McLean C<< <grantm@cpan.org> >>
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1;
+
